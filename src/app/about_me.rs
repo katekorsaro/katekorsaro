5f17895a3b27c6cf4284c014e0aa62c9b4128700
Kate Korsaro <kate.korsaro@gmail.com>
2024-02-06 08:25:37 +0100

    about me, two columns with some info

--- src/app/about_me.rs
+++ src/app/about_me.rs
@@ -4,10 +4,30 @@ use leptos::{component,view,IntoView};
 pub fn AboutMe () -> impl IntoView {
     view! {
         <div class="container mt-8 mx-auto">
-            <h1 class="text-lg text-pink-500">"About Me"</h1>
-            <ul class="list-disc ms-4">
-                <li>"Kate Korsaro here, obviously a fake name :)"</li>
-            </ul>
+            <h1 class="text-lg text-pink-500 font-semibold">"About Me"</h1>
+
+            <div class="columns-2">
+                <ul class="list-disc ms-4">
+                    <li>"Kate Korsaro here, obviously a " <u>"fake name"</u> " :)"</li>
+                    <li>
+                        <u>"Rust"</u>
+                        " programming language"
+                    </li>
+                    <li>"CLI utilities"</li>
+                    <li>
+                        <u>"Linux"</u>
+                        " (currently running a heavy customised Debian 12)"
+                    </li>
+                    <li>"Web development (not much to be honest, but it's trendy)"</li>
+                    <li>
+                        <u>"Roleplaying"</u>
+                        " games"
+                    </li>
+                    <li>"Working as a full time developer and team lead"</li>
+                    <li>"And in my spare time"</li>
+                </ul>
+            </div>
+
         </div>
     }
 }
