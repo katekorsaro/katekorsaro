d324e136cd2a4a5344d5b55fae4ea3bd36e0e89c
Kate Korsaro <kate.korsaro@gmail.com>
2024-02-06 16:20:20 +0100

    nav component init

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,18 @@
 mod about_me;
 mod hero;
+mod nav;
 mod projects;
 
 use about_me::*;
 use hero::*;
 use leptos::{component, view, IntoView};
+use nav::*;
 use projects::*;
 
 #[component]
 pub fn App() -> impl IntoView {
     view! {
+        <Nav/>
         <Hero/>
         <AboutMe/>
         <Projects/>
---
+++ src/app/nav.rs
@@ -0,0 +1,17 @@
+use leptos::{component, view, IntoView};
+
+#[component]
+pub fn Nav() -> impl IntoView {
+    view! {
+        <div class="container mx-auto text-right mt-4">
+            <ul>
+                <li class="inline-block underline bg-slate-800 p-2 rounded rounded-2 shadow">
+                    <a href="#">"Home"</a>
+                </li>
+                <li class="inline underline ml-4">
+                    <a href="#">"Blog (coming soon)"</a>
+                </li>
+            </ul>
+        </div>
+    }
+}
