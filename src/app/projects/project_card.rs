3aabdddaa20d3f1128f6efd83448e83b7feffa15
Kate Korsaro <kate.korsaro@gmail.com>
2024-02-06 15:31:10 +0100

    title with url to project url

--- src/app/projects/project_card.rs
+++ src/app/projects/project_card.rs
@@ -3,6 +3,8 @@ use leptos::{component, view, IntoView};
 
 #[component]
 pub fn ProjectCard(project: Project) -> impl IntoView {
+    let url = project.project_url.clone();
+
     view! {
         <div
             class=("border-red-500", move || project.status_tag == ProjectStatus::Red)
@@ -11,7 +13,12 @@ pub fn ProjectCard(project: Project) -> impl IntoView {
             class=("border-cyan-500", move || project.status_tag == ProjectStatus::Cyan)
             class="bg-slate-800 rounded rounded-4 p-2 border-s-4"
         >
-            <h1 class="text-cyan-500">{move || project.title.clone()}</h1>
+            <h1
+                class="text-cyan-500"
+                class=("underline", move || url.is_some())
+            >
+                <a href=move || project.project_url.clone()>{move || project.title.clone()}</a>
+            </h1>
             <p class="text-xs text-slate-400">"Status: " {move || project.status.clone()}</p>
             <p class="mt-2">{move || project.description.clone()}</p>
             <p class="text-xs text-slate-500 mt-2">
@@ -25,6 +32,7 @@ pub fn ProjectCard(project: Project) -> impl IntoView {
                         .map(|x| format!("#{}", x))
                         .reduce(|acc, e| format!("{acc} {e}"))
                 }}
+
             </p>
         </div>
     }
