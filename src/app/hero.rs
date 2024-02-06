2779122cd5747faa0fc4067b7ffe24db0549afe4
Kate Korsaro <kate.korsaro@gmail.com>
2024-02-06 08:46:07 +0100

    hero format and fonts

--- src/app/hero.rs
+++ src/app/hero.rs
@@ -4,8 +4,8 @@ use leptos::{component, view, IntoView};
 pub fn Hero() -> impl IntoView {
     view! {
         <div class="container mt-16 mx-auto">
-            <h1 class="text-pink-500 text-xl">"Kate Korsaro"</h1>
-            <p>A messy dev</p>
+            <h1 class="text-pink-500 text-2xl font-semibold">"Kate Korsaro"</h1>
+            <p class="text-lg">A messy dev</p>
         </div>
     }
 }
