045e5c0dcf546ee9cddf2fbe821ccc8e76267159
Kate Korsaro <kate.korsaro@gmail.com>
2024-02-06 08:43:53 +0100

    projects component first draft

--- src/app.rs
+++ src/app.rs
@@ -5,11 +5,13 @@ mod projects;
 use about_me::*;
 use hero::*;
 use leptos::{component, view, IntoView};
+use projects::*;
 
 #[component]
 pub fn App() -> impl IntoView {
     view! {
         <Hero/>
         <AboutMe/>
+        <Projects/>
     }
 }
--- src/app/projects.rs
+++ src/app/projects.rs
@@ -1,1 +1,10 @@
+use leptos::{component, view, IntoView};
 
+#[component]
+pub fn Projects() -> impl IntoView {
+    view! {
+        <div class="container mt-8 mx-auto">
+            <h1 class="text-lg text-pink-500 font-semibold">"Current Projects"</h1>
+        </div>
+    }
+}
