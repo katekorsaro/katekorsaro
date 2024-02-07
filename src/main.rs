1c09719d207bd6f3e1e5dd04295183a4e2eb2898
Kate Korsaro <kate.korsaro@gmail.com>
2024-02-07 12:37:12 +0100

    no global reexport

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ mod app;
 mod data;
 mod state;
 
-pub use app::*;
-pub use data::*;
-pub use state::*;
+use app::*;
+use data::*;
+use state::*;
 
-pub use leptos::*;
-pub use leptos_router::*;
+use leptos::*;
+use leptos_router::*;
 
 fn main() {
     leptos::mount_to_body(|| view! { <App/> })
