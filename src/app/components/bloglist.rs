a5e3b18b85cd70294332094d3d7ce0420e77bf10
Kate Korsaro <kate.korsaro@gmail.com>
2024-02-08 12:30:13 +0100

    linebreak to paragraph in html

--- src/app/components/bloglist.rs
+++ src/app/components/bloglist.rs
@@ -7,12 +7,24 @@ pub fn BlogList() -> impl IntoView {
             let mut blogposts = list_of_all_blogposts();
             blogposts.sort_by(|a, b| b.date.cmp(&a.date));
             blogposts
-                .into_iter()
+                .iter()
                 .map(|blogpost| {
                     view! {
-                        <h1 class="dark:text-rose-500 text-rose-900 text-xl font-semibold">{&blogpost.title}</h1>
-                        <p class="dark:text-slate-400 text-slate-600 text-xs mb-4">"published on: " {&blogpost.formatted_date()}</p>
-                        <p class="mb-16">{blogpost.text}</p>
+                        <h1 class="dark:text-rose-500 text-rose-900 text-xl font-semibold">
+                            {&blogpost.title}
+                        </h1>
+                        <p class="dark:text-slate-400 text-slate-600 text-xs mb-4">
+                            "published on: " {&blogpost.formatted_date()}
+                        </p>
+
+                        {&blogpost
+                            .text
+                            .split("\n")
+                            .map(|split| {
+                                let line = String::from(split);
+                                view! { <p class="mt-2">{line}</p> }
+                            })
+                            .collect_view()}
                     }
                 })
                 .collect_view()
--- src/data/blogpost.rs
+++ src/data/blogpost.rs
@@ -32,26 +32,21 @@ pub fn list_of_all_blogposts() -> Vec<BlogPost> {
             "Hey there, fellow wanderers of the digital realm! Today marks the dawn of a new chapter in my coding \
             saga – diving headfirst into the vibrant sea of open source development. As a humble software dev, \
             I've decided it's high time to roll up my sleeves and contribute to the ever-expanding cosmos of \
-            code. \
-             \
+            code.\n\
             So, what's got me buzzing with excitement lately? Well, I've found myself knee-deep in the world of \
             Leptos – no, not the diet pills, but rather the captivating open-source project that's been tickling \
             my coding fancy. And guess what? This little site you're browsing right now is the fruit of that \
-            laborious tinkering. \
-             \
+            laborious tinkering.\n\
             Now, before you start picturing me as some kind of coding superhero, let me set the record straight \
             – I'm just your average dev, armed with nothing but a keyboard, a cup of coffee, and a dash of \
-            determination. But hey, who needs a cape when you've got code, right? \
-             \
+            determination. But hey, who needs a cape when you've got code, right?\n\
             Sure, I may be new to this whole open source gig, but I've got a fire in my belly and a willingness \
             to learn that's second to none. So, if you're a seasoned open source vet stumbling upon this humble \
             abode, consider this an open invitation to lend me your wisdom. And if you're a fellow newbie like \
-            myself, well, let's strap ourselves in and embark on this wild ride together. \
-             \
+            myself, well, let's strap ourselves in and embark on this wild ride together.\n\
             They say every journey begins with a single step, and consider this blog post my leap into the \
             unknown. Who knows where this path will lead? But one thing's for sure – I'm ready to roll up my \
-            sleeves, get my hands dirty, and make my mark on the open source world, one line of code at a time. \
-             \
+            sleeves, get my hands dirty, and make my mark on the open source world, one line of code at a time.\n\
             So here's to new beginnings, endless possibilities, and the thrilling adventure that lies ahead. \
             Let's make some magic happen, shall we?"
         ),
