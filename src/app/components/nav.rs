1c5381d43b45b0e196833dd22bca58e19b1b58df
Kate Korsaro <kate.korsaro@gmail.com>
2024-02-08 09:46:05 +0100

    refactor of navs into an iteration

--- src/app/components/nav.rs
+++ src/app/components/nav.rs
@@ -3,31 +3,48 @@ use crate::*;
 #[component]
 pub fn Nav() -> impl IntoView {
     let (state, _set_state) = use_state();
+    let navs = vec![
+        ("Home", &SelectedTab::Home, "/"),
+        ("Blog", &SelectedTab::Blog, "/blog"),
+    ];
     view! {
         <div class="container mx-auto text-right mt-4">
             <ul>
-                <li
-                    class="inline-block p-2 rounded"
-                    class=("underline", move || state.with(|s| s.navigation.selected_tab == SelectedTab::Home))
-                    class=("decoration-2", move || state.with(|s| s.navigation.selected_tab == SelectedTab::Home))
-                    class=("underline-offset-2", move || state.with(|s| s.navigation.selected_tab == SelectedTab::Home))
-                    class=("decoration-rose-900", move || state.with(|s| s.navigation.selected_tab == SelectedTab::Home))
-                    class=("dark:decoration-rose-500", move || state.with(|s| s.navigation.selected_tab == SelectedTab::Home))
-                >
 
-                    <a href="/">"Home"</a>
-                </li>
-                <li
-                    class="inline-block p-2 rounded"
-                    class=("underline", move || state.with(|s| s.navigation.selected_tab == SelectedTab::Blog))
-                    class=("decoration-2", move || state.with(|s| s.navigation.selected_tab == SelectedTab::Blog))
-                    class=("underline-offset-2", move || state.with(|s| s.navigation.selected_tab == SelectedTab::Blog))
-                    class=("decoration-rose-900", move || state.with(|s| s.navigation.selected_tab == SelectedTab::Blog))
-                    class=("dark:decoration-rose-500", move || state.with(|s| s.navigation.selected_tab == SelectedTab::Blog))
-                >
+                {navs
+                    .into_iter()
+                    .map(|nav| {
+                        view! {
+                            <li
+                                class="inline-block p-2 rounded"
+                                class=(
+                                    "underline",
+                                    move || { state.with(|s| s.navigation.selected_tab == *nav.1) },
+                                )
+                                class=(
+                                    "decoration-2",
+                                    move || { state.with(|s| s.navigation.selected_tab == *nav.1) },
+                                )
+                                class=(
+                                    "underline-offset-2",
+                                    move || { state.with(|s| s.navigation.selected_tab == *nav.1) },
+                                )
+                                class=(
+                                    "decoration-rose-900",
+                                    move || { state.with(|s| s.navigation.selected_tab == *nav.1) },
+                                )
+                                class=(
+                                    "dark:decoration-rose-500",
+                                    move || { state.with(|s| s.navigation.selected_tab == *nav.1) },
+                                )
+                            >
 
-                    <a href="/blog">"Blog"</a>
-                </li>
+                                <a href=nav.2>{nav.0}</a>
+                            </li>
+                        }
+                    })
+                    .collect_view()}
+
             </ul>
         </div>
     }
