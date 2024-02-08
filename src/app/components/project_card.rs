8c4dcac2924d72a8d578983cc9e107ed5d014f80
Kate Korsaro <kate.korsaro@gmail.com>
2024-02-08 08:08:29 +0100

    status color for light theme

--- src/app/components/project_card.rs
+++ src/app/components/project_card.rs
@@ -6,10 +6,11 @@ pub fn ProjectCard(project: ReadSignal<Project>) -> impl IntoView {
         <div
             class=("border-red-500", move || project.get().status_tag == ProjectStatus::Red)
             class=("border-amber-500", move || project.get().status_tag == ProjectStatus::Amber)
-            class=("border-emerald-500", move || project.get().status_tag == ProjectStatus::Emerald)
+            class=("border-lime-500", move || project.get().status_tag == ProjectStatus::Emerald)
             class=("border-cyan-500", move || project.get().status_tag == ProjectStatus::Cyan)
             class="bg-slate-300 rounded rounded-4 p-2 border-s-4 shadow-md"
         >
+            <!-- "card title" -->
             <h2
                 class="text-slate-800 text-lg font-semibold"
                 class=("underline", move || project.get().project_url.is_some())
@@ -18,8 +19,11 @@ pub fn ProjectCard(project: ReadSignal<Project>) -> impl IntoView {
                     project.get().project_url.clone()
                 }>{move || project.get().title.clone()}</a>
             </h2>
-            <p class="text-slate-400 text-xs">"Status: " {move || project.get().status.clone()}</p>
+            <!-- "status" -->
+            <p class="text-slate-600 text-xs">"Status: " {move || project.get().status.clone()}</p>
+            <!-- "description" -->
             <p class="mt-2">{move || project.get().description.clone()}</p>
+            <!-- "tags" -->
             <p class="text-slate-500 mt-2 text-xs">
 
                 {move || {
