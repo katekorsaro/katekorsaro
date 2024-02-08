1861ae5087efefa8e00e26ea8400e69c86c17034
Kate Korsaro <kate.korsaro@gmail.com>
2024-02-08 11:44:50 +0100

    format and first blog post

--- src/data/blogpost.rs
+++ src/data/blogpost.rs
@@ -13,15 +13,47 @@ impl BlogPost {
             text: String::from(text),
         }
     }
-    pub fn formatted_date (&self) -> String {
+    pub fn formatted_date(&self) -> String {
         let date_str = self.date.to_string();
-        format!("{}/{}/{}", date_str[..4].to_string(), date_str[4..6].to_string(), date_str[6..8].to_string())
+        format!(
+            "{}/{}/{}",
+            date_str[..4].to_string(),
+            date_str[4..6].to_string(),
+            date_str[6..8].to_string()
+        )
     }
 }
 
 pub fn list_of_all_blogposts() -> Vec<BlogPost> {
     vec![
-        BlogPost::new("Title", 20240204, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel pretium lectus quam id leo in vitae turpis. Dignissim convallis aenean et tortor at risus viverra adipiscing. Varius quam quisque id diam vel. Fermentum dui faucibus in ornare. Faucibus a pellentesque sit amet porttitor eget dolor. Lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. Nisl pretium fusce id velit ut. Sed felis eget velit aliquet sagittis id consectetur. Volutpat est velit egestas dui id ornare arcu odio. Massa ultricies mi quis hendrerit. Proin sed libero enim sed faucibus turpis in eu. Eget felis eget nunc lobortis mattis aliquam faucibus. Nibh sit amet commodo nulla. Tempor nec feugiat nisl pretium fusce id velit ut. Nisi vitae suscipit tellus mauris a diam maecenas. Placerat in egestas erat imperdiet sed euismod nisi porta lorem. Sit amet mattis vulputate enim nulla aliquet. Velit ut tortor pretium viverra suspendisse potenti nullam ac. Sagittis orci a scelerisque purus semper. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros in. Nibh cras pulvinar mattis nunc sed blandit libero volutpat. Dignissim sodales ut eu sem integer. Eu feugiat pretium nibh ipsum consequat. Morbi enim nunc faucibus a pellentesque sit amet porttitor. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Massa massa ultricies mi quis hendrerit dolor magna eget est. Risus quis varius quam quisque id diam. Cursus turpis massa tincidunt dui ut ornare lectus sit. Etiam dignissim diam quis enim lobortis scelerisque. Leo duis ut diam quam nulla porttitor massa. Elit pellentesque habitant morbi tristique senectus et netus et. Amet facilisis magna etiam tempor orci eu lobortis elementum. Feugiat nisl pretium fusce id velit ut. Rhoncus urna neque viverra justo nec ultrices. Amet nisl purus in mollis nunc. Mollis nunc sed id semper risus in hendrerit gravida."),
-        BlogPost::new("Title", 20240208, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel pretium lectus quam id leo in vitae turpis. Dignissim convallis aenean et tortor at risus viverra adipiscing. Varius quam quisque id diam vel. Fermentum dui faucibus in ornare. Faucibus a pellentesque sit amet porttitor eget dolor. Lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. Nisl pretium fusce id velit ut. Sed felis eget velit aliquet sagittis id consectetur. Volutpat est velit egestas dui id ornare arcu odio. Massa ultricies mi quis hendrerit. Proin sed libero enim sed faucibus turpis in eu. Eget felis eget nunc lobortis mattis aliquam faucibus. Nibh sit amet commodo nulla. Tempor nec feugiat nisl pretium fusce id velit ut. Nisi vitae suscipit tellus mauris a diam maecenas. Placerat in egestas erat imperdiet sed euismod nisi porta lorem. Sit amet mattis vulputate enim nulla aliquet. Velit ut tortor pretium viverra suspendisse potenti nullam ac. Sagittis orci a scelerisque purus semper. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros in. Nibh cras pulvinar mattis nunc sed blandit libero volutpat. Dignissim sodales ut eu sem integer. Eu feugiat pretium nibh ipsum consequat. Morbi enim nunc faucibus a pellentesque sit amet porttitor. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Massa massa ultricies mi quis hendrerit dolor magna eget est. Risus quis varius quam quisque id diam. Cursus turpis massa tincidunt dui ut ornare lectus sit. Etiam dignissim diam quis enim lobortis scelerisque. Leo duis ut diam quam nulla porttitor massa. Elit pellentesque habitant morbi tristique senectus et netus et. Amet facilisis magna etiam tempor orci eu lobortis elementum. Feugiat nisl pretium fusce id velit ut. Rhoncus urna neque viverra justo nec ultrices. Amet nisl purus in mollis nunc. Mollis nunc sed id semper risus in hendrerit gravida.")
+        BlogPost::new(
+            "A humble beginning...",
+            20240208,
+            "Hey there, fellow wanderers of the digital realm! Today marks the dawn of a new chapter in my coding \
+            saga – diving headfirst into the vibrant sea of open source development. As a humble software dev, \
+            I've decided it's high time to roll up my sleeves and contribute to the ever-expanding cosmos of \
+            code. \
+             \
+            So, what's got me buzzing with excitement lately? Well, I've found myself knee-deep in the world of \
+            Leptos – no, not the diet pills, but rather the captivating open-source project that's been tickling \
+            my coding fancy. And guess what? This little site you're browsing right now is the fruit of that \
+            laborious tinkering. \
+             \
+            Now, before you start picturing me as some kind of coding superhero, let me set the record straight \
+            – I'm just your average dev, armed with nothing but a keyboard, a cup of coffee, and a dash of \
+            determination. But hey, who needs a cape when you've got code, right? \
+             \
+            Sure, I may be new to this whole open source gig, but I've got a fire in my belly and a willingness \
+            to learn that's second to none. So, if you're a seasoned open source vet stumbling upon this humble \
+            abode, consider this an open invitation to lend me your wisdom. And if you're a fellow newbie like \
+            myself, well, let's strap ourselves in and embark on this wild ride together. \
+             \
+            They say every journey begins with a single step, and consider this blog post my leap into the \
+            unknown. Who knows where this path will lead? But one thing's for sure – I'm ready to roll up my \
+            sleeves, get my hands dirty, and make my mark on the open source world, one line of code at a time. \
+             \
+            So here's to new beginnings, endless possibilities, and the thrilling adventure that lies ahead. \
+            Let's make some magic happen, shall we?"
+        ),
     ]
 }
